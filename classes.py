4eed9e8503089efe2031ced0757ddf2750e11d7a
Simon <ndungusimon564@gmail.com>
2025-02-17 08:18:11 +0300

    complete

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Dog:
+
+    def bark(self):
+        print("wolf!")
+
+    def sit(self):
+        print("The dog is sitting")
+
+     
---
+++ person.py
@@ -0,0 +1,7 @@
+class Person:
+    def talk(self):
+        print ("Hello World!")
+
+
+    def walk(self):
+        print("The Person is walking")
